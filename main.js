3a01d572d365524c540753e0b32b404dca6381e1
Matrosdzmehedi <matrosdzmehedi111@gmail.com>
2021-01-10 16:53:21 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
 
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('cached_service_worker.js')
+        navigator.serviceWorker.register('service_worker.js')
             .then(reg => console.log('founded'))
             .catch(err => console.log(`error:${err}`));
     });
